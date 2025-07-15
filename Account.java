e0f29e8c481cecef06efe5405bb1cce8335b0516
Ajay0738 <boddupalliajay844@gmail.com>
2025-07-15 12:41:02 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,10 @@
+class Account{
+void deposit(double amount){
+//code to deposit the specified amount
+System.out.println("Desposited:"+amount);
+}
+public static void main(string[]args){
+Account myAccount=new Account();
+myAccount.deposit(amount100.0);
+}
+}
