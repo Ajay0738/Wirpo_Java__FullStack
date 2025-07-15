1e765d809aa3a41a7a1c8e7742dd94e67c522f05
Ajay0738 <boddupalliajay844@gmail.com>
2025-07-15 20:21:14 +0530

    ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class ASCII {
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+System.out.print("Enter the number:");
+char var='A';
+System.out.println((int)var);
+if(var>='a'&& var<='z'){
+System.out.println("Lowercase");
+}
+else{
+System.out.println("Uppercase");
+}
+}
+}
