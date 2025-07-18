cabd07cec5e8ef43317de87d6d7f9b898e1737c7
Ajay0738 <boddupalliajay844@gmail.com>
2025-07-18 11:52:53 +0530

    Wrapper.java

---
+++ Wrapper.java
@@ -0,0 +1,32 @@
+package Wrapper;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Integer value");
+		System.out.println("min:"+Integer.MIN_VALUE);
+		System.out.println("max:"+Integer.MAX_VALUE);
+		
+		System.out.println("Double value");
+		System.out.println("min:"+Double.MIN_VALUE);
+		System.out.println("max:"+Double.MAX_VALUE);
+		
+		System.out.println("Long value");
+		System.out.println("min:"+Long.MIN_VALUE);
+		System.out.println("max:"+Long.MAX_VALUE);
+		
+		System.out.println("Float value");
+		System.out.println("min:"+Float.MIN_VALUE);
+		System.out.println("max:"+Float.MAX_VALUE);
+		
+		System.out.println("Byte value");
+		System.out.println("min:"+Byte.MIN_VALUE);
+		System.out.println("max:"+Byte.MAX_VALUE);
+		
+		System.out.println("Short value");
+		System.out.println("min:"+Short.MIN_VALUE);
+		System.out.println("max:"+Short.MAX_VALUE);
+	}
+
+}
