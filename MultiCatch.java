acf82cb17ca18443b849dbdc368386b099596c4c
Ajay0738 <boddupalliajay844@gmail.com>
2025-07-18 10:11:23 +0530

    MultiCatch.java

---
+++ MultiCatch.java
@@ -0,0 +1,23 @@
+package Multicatch;
+
+public class MultiCatch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int a=args.length;
+			System.out.println("1="+1);
+			int b=42/1;
+			int arr[]= {1};
+			arr[22]=99;
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Divide by 0:"+e);
+		}
+	    catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array index oob:"+e);
+		}
+		System.out.println("After try/catch blocks");
+			}
+
+		}
